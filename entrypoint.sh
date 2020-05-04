6a6e9dd5595f347f238f11df7d11542d3877c682
Michal Haltuf <haltuf@imagineo.cz>
2020-05-04 10:24:00 +0200

    do not pass input_config_file twice, allow for docker run without env variables set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-cp /ftp-deployment/provide-config.php provide-config.php
-deployment provide-config.php $*
-rm provide-config.php
+set -e
+
+if [ ${INPUT_CONFIG_FILE} ]; then
+  cp /ftp-deployment/provide-config.php provide-config.php
+  deployment provide-config.php ${INPUT_PARAMETERS} $@
+  rm provide-config.php
+else
+  deployment $*
+fi
